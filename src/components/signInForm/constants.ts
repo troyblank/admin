c1ea7948a664f7c58d5faeb03f23d40afdb068b8
Troy Blank <troy@troyblank.com>
2023-07-28 21:25:05 -0500

    adds unit test for low level components

--- src/components/signInForm/constants.ts
+++ src/components/signInForm/constants.ts
@@ -1,1 +1,4 @@
 export const SIGN_IN_HEADER: string = 'Please sign in.'
+export const USER_NAME_ID: string = 'username'
+export const PASSWORD_ID: string = 'password'
+export const SUBMIT_LABEL: string = 'Submit'
