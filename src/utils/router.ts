f9544e46f28ffd75518608f48b21f33999891101
Troy Blank <troy@troyblank.com>
2023-10-06 15:43:23 -0500

    fixes auth provider for new users

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,3 +1,3 @@
 export const HOME_PATH: string = '/'
-export const COMPLETE_USER_PATH: string = '/completeUser'
+export const COMPLETE_USER_PATH: string = '/completeNewUser'
 export const SIGN_IN_PATH: string = '/signIn'
